d3f0387ff7bdf8fa90b6f129504fb21452c1527f
MatheusOliveira2 <matheusalmeida.soliveira@gmail.com>
2023-07-20 23:08:36 +1000

    removing stories examples

--- cosette-code-challenge/src/components/pages/catalogue/Catalogue.stories.ts
+++ cosette-code-challenge/src/components/pages/catalogue/Catalogue.stories.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { expect } from "@storybook/jest";
 import type { Meta, StoryObj } from "@storybook/react";
 
